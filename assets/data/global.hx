1554d3b65283e3f9c3ec11651f929b29e7d95173
Chronic x3 <126361810+chronicsilly@users.noreply.github.com>
2025-06-12 08:59:51 +0100

    Update global.hx

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,3 +1,8 @@
 //
+import funkin.backend.system.modules.FunkinCache;
+
+function new() {
+    FunkinCache.instance.clearSecondLayer();
+
 function update(elapsed:Float)
 	if (FlxG.keys.justPressed.F5) FlxG.resetState();
